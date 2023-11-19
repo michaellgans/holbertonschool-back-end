c069e6411273cda47f024801a08214c52896ce16
michaellgans <michaellgans@Victoria>
2023-11-19 17:56:01 -0600

    Creates task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Task 0 - Gather data from API """
+
+import requests
+import sys
+
+
+def employee_tasks(ID):
+    """ Returns all tasks of an employee """
+    url = "https://jsonplaceholder.typicode.com/"
+    url += "users/{}/todos".format(ID)
+    response = requests.get(url)
+    return response.json()
+
+
+def employee_name(ID):
+    """ Returns name of employee """
+    url = "https://jsonplaceholder.typicode.com/"
+    url += "users/{}".format(ID)
+    response = requests.get(url)
+    return response.json().get("name")
+
+
+def completed_tasks(tasks):
+    """ Returns completed tasks """
+    completed_tasks = []
+
+    for item in tasks:
+        if item.get("completed"):
+            completed_tasks.append(task)
+    return completed_tasks
+
+
+def print_report(employee_name, tasks_completed, tasks_total):
+    """ Displays report of tasks by employee ID """
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, len(tasks_completed), len(total_tasks)))
+    for item in completed_tasks:
+        print("t/ {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    ID = sys.argv[1]
+    tasks = employee_tasks(ID)
+    employee_name = employee_name(ID)
+    completed_tasks = tasks_completed(tasks)
+    print_report(employee_name, completed_tasks, len(tasks))
